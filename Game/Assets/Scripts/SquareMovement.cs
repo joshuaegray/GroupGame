bde6b37b8688e8cacac9a404783a5e5c4445debc
Ribhav-beep <75393113+Ribhav-beep@users.noreply.github.com>
2023-11-14 09:30:10 -0800

    changed description

--- Game/Assets/Scripts/SquareMovement.cs
+++ Game/Assets/Scripts/SquareMovement.cs
@@ -4,7 +4,7 @@ public class SquareMovement : MonoBehaviour
 {
     [Header("Movement variables")]
     public float moveSpeed;         // constant movement speed
-    public float xmin;              // x position limit that triggers reset leftwards
+    public float xmin;              // x position limit that triggers reset rightwards
     public Vector3 respawnPosition; // position object will reset to
   
     void Update()
