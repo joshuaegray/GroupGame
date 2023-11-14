9ef1829a426015730298a9824d42ce95b5b978c5
joshuaegray <100026611@mvla.net>
2023-11-14 09:42:17 -0800

    Added Collision Detection Script to Player

---
+++ Game/Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    public float rayLength = 1;
+
+    public LayerMask physicsLayersToSense;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics2D.Raycast(transform.position, transform.right, rayLength, physicsLayersToSense))
+        {
+            Destroy(this, 0);
+        }
+        
+    }
+}
