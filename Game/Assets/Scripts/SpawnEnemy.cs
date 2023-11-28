b803273b2afe4bb1f3ac63a907885f0b449f60a1
joshuaegray <100026611@mvla.net>
2023-11-28 09:40:32 -0800

    Changed Script to try to spawn walls and balls

--- Game/Assets/Scripts/SpawnEnemy.cs
+++ Game/Assets/Scripts/SpawnEnemy.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 
 public class SpawnEnemy : MonoBehaviour
 {
-	public GameObject enemyPrefab;
+	public GameObject[] enemyPrefabs;
+	private int stage;
+	
 	public float startingInterval, minimumInterval, intervalPercentDampening;
 	public float minX, maxX, minY, maxY;
 
 	[SerializeField] private float interval;
+	
+	
+	
+	
 
    void Start()
    {
+	   stage = SceneManager.GetActiveScene().buildIndex;
 	   interval = startingInterval;
 	   StartCoroutine(SpawnBalls());
    }
@@ -21,13 +30,14 @@ public class SpawnEnemy : MonoBehaviour
    {
 	   while (true)
 	   {
+		   int enemyNum = Random.Range(0, enemyPrefabs.Length-1);
 		   yield return new WaitForSeconds(interval);
 		   //Debug.Log("Updating now: " + interval);
 		   interval = (interval - minimumInterval) * intervalPercentDampening + minimumInterval;
 		   float randX = Random.Range(minX, maxX);
 		   float randY = Random.Range(minY, maxY);
 		   Vector3 spawnPos = new Vector3(randX, randY, 0f);
-		   Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+		   Instantiate(enemyPrefabs[enemyNum], spawnPos, Quaternion.identity);
 	   }
    }
 }
