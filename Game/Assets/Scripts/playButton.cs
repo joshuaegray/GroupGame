404883dfbb4043693ee375626a91d8b3f19b00d6
Ribhav-beep <75393113+Ribhav-beep@users.noreply.github.com>
2023-12-05 09:55:54 -0800

    FINALLY FIXED IT WITH ALL THE BLOOD, SWEAT, AND TEARS OF STRUGGLING!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    FINALLY FIXED IT WITH ALL THE BLOOD, SWEAT, AND TEARS OF STRUGGLING!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ Game/Assets/Scripts/playButton.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playButton : MonoBehaviour
+{
+    // The public function the button will call when it is clicked
+    public void loadLevel(string levelName)
+    {
+        // Takes in a string level name and uses it to load that level
+        SceneManager.LoadScene(levelName);
+    }
+}
