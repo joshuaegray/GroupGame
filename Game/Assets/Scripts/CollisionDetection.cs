b9690a83d05ad93da477737930f6a08d7cad1271
joshuaegray <100026611@mvla.net>
2023-12-12 09:37:55 -0800

    Fixed Collision

--- Game/Assets/Scripts/CollisionDetection.cs
+++ Game/Assets/Scripts/CollisionDetection.cs
@@ -8,14 +8,24 @@ public class CollisionDetection : MonoBehaviour
 {
     private float rayLength = 1;
 
-    public LayerMask physicsLayersToSense;
+   // public LayerMask physicsLayersToSense;
+
+    public string deathTag;
     // Start is called before the first frame update
-    void Start()
+
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        
+        if (other.gameObject.CompareTag(deathTag))
+        {
+            Debug.Log("HIT");
+            Destroy(this.gameObject, 0);
+            SceneManager.LoadScene("Death Screen");
+        }
     }
 
     // Update is called once per frame
+    
+    /*
     void Update()
     {
         if (Physics2D.Raycast(transform.position, transform.right, rayLength, physicsLayersToSense))
@@ -25,6 +35,8 @@ public class CollisionDetection : MonoBehaviour
         }
         
     }
+    
+    */
 }
 
 
